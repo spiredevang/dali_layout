b479a82ba340c7cd1dbefa451b617d0c1223af84
Devang Patel <devang@spiretrading.com>
2022-03-15 19:07:47 -0400

    Add json upload to HomePage

--- src/components/home_page.tsx
+++ src/components/home_page.tsx
@@ -2,20 +2,40 @@ import * as React from 'react';
 
 interface Properties {}
 
-interface State {}
+interface State {
+  jsonContents: any;
+}
 
 /** The home page component. */
 export class HomePage extends React.Component<Properties, State> {
   constructor(props: Properties) {
     super(props)
-    this.state = {};
+    this.state = {jsonContents: {}};
   }
 
   public render(): JSX.Element {
+    console.log('render', this.state.jsonContents);
     return (
       <div style={HomePage.STYLE.container}>
         <h1>Layout Application</h1>
+        <input type='file' onChange={this.onChange}/>
       </div>);
+  }
+
+  private onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files as any
+    const file = files[0];
+    if(file.type === 'application/json') {
+      const readFile = new FileReader();
+      readFile.onload = this.onLoad;
+      readFile.readAsText(file);
+    }
+  }
+
+  private onLoad = (e: any) => {
+    const contents = e.target.result;
+    const jsonContents = JSON.parse(contents);
+    this.setState({jsonContents})
   }
 
   private static readonly STYLE = {
