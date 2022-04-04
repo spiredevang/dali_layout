252c770c42e143f3fe9a6b500585b1e4a7e7c19e
Devang Patel <devang@spiretrading.com>
2022-04-04 16:48:26 -0400

    Add width and height input fields to FlexLayoutViewer

--- src/components/flex_layout_viewer.tsx
+++ src/components/flex_layout_viewer.tsx
@@ -10,15 +10,35 @@ interface Properties {
   limits: Limits;
 }
 
+interface State {
+  isWidthEnabled: boolean;
+  isHeightEnabled: boolean;
+  width: number;
+  height: number;
+}
+
 /** The flex layout viewer component. */
-export class FlexLayoutViewer extends React.Component<Properties> {
+export class FlexLayoutViewer extends React.Component<Properties, State> {
+  constructor(props: Properties) {
+    super(props);
+    const {minimumWidth, minimumHeight, maximumWidth, maximumHeight} = this.props.limits;
+    const width = minimumWidth === maximumWidth ? minimumWidth : minimumWidth * 1.2;
+    const height = minimumHeight === maximumHeight ? minimumHeight : minimumHeight * 1.2;
+    this.state = {
+      isWidthEnabled: false,
+      isHeightEnabled: false,
+      width,
+      height
+    };
+  }
+
   public render(): JSX.Element {
     const isRow = this.props.orientation === Orientation.ROW;
     const flexContainerDirection = isRow && 'column' || 'row';
     const flexDirection = isRow && 'row' || 'column';
     const gap = isRow && '1px 0' || '0 1px';
     const dim = isRow && {width: '100%', columnGap: 1} || {height: '100%', rowGap: 1};
-    const [width, height] = (() => {
+    const [widthLimit, heightLimit] = (() => {
       if(this.props.limits) {
         const {minimumWidth, minimumHeight, maximumWidth, maximumHeight} = this.props.limits;
         const width = `clamp(${minimumWidth}px, 100%, ${maximumWidth}px)`;
@@ -28,27 +48,57 @@ export class FlexLayoutViewer extends React.Component<Properties> {
         return ['100%, 100%'];
       }
     })();
+    const width = this.state.isWidthEnabled ? this.state.width : widthLimit;
+    const height = this.state.isHeightEnabled ? this.state.height : heightLimit;
     return (
-      <div style={{...FlexLayoutViewer.STYLE.container, width, height,
-          flexDirection: flexContainerDirection, gap}}>
-        {this.props.rectangleMatrix?.map((set, index) => {
-          const isWidthFlexible = set.every(rect =>
-            rect.horizontalPolicy === Constraint.FILL_SPACE);
-          const isHeightFlexible = set.every(rect =>
-            rect.verticalPolicy === Constraint.FILL_SPACE);
-          const flex = (() => {
-            if(isRow) {
-              return isHeightFlexible && '1 1 auto' || '0 0 auto';
-            } else {
-              return isWidthFlexible && '1 1 auto' || '0 0 auto';
-            }
-          })();
-          return (
-            <div style={{display: 'flex', flexDirection, flex, ...dim}} key={index}>
-              {set.map(this.renderRectangle)}
-            </div>);
-        })}
-    </div>);
+      <div style={FlexLayoutViewer.STYLE.wrapper}>
+        <div style={{...FlexLayoutViewer.STYLE.container, width, height,
+            flexDirection: flexContainerDirection, gap}}>
+          {this.props.rectangleMatrix?.map((set, index) => {
+            const isWidthFlexible = set.every(rect =>
+              rect.horizontalPolicy === Constraint.FILL_SPACE);
+            const isHeightFlexible = set.every(rect =>
+              rect.verticalPolicy === Constraint.FILL_SPACE);
+            const flex = (() => {
+              if(isRow) {
+                return isHeightFlexible && '1 1 auto' || '0 0 auto';
+              } else {
+                return isWidthFlexible && '1 1 auto' || '0 0 auto';
+              }
+            })();
+            return (
+              <div style={{display: 'flex', flexDirection, flex, ...dim}} key={index}>
+                {set.map(this.renderRectangle)}
+              </div>);
+          })}
+        </div>
+        <div style={FlexLayoutViewer.STYLE.inputContainer}>
+          <div style={FlexLayoutViewer.STYLE.inputGroup}>
+            <input type='checkbox' checked={this.state.isWidthEnabled}
+              onChange={this.onChangeWidthCheckbox}/>
+            <label htmlFor='width' style={{marginRight: 5}}>Width</label>
+            <input
+              type='number'
+              id='width'
+              value={this.state.width}
+              min={this.props.limits.minimumWidth} 
+              max={this.props.limits.maximumWidth} 
+              onChange={this.onChangeWidth}/>
+          </div>
+          <div style={FlexLayoutViewer.STYLE.inputGroup}>
+            <input type='checkbox' checked={this.state.isHeightEnabled}
+              onChange={this.onChangeHeightCheckbox}/>
+            <label htmlFor='height' style={{marginRight: 5}}>Height</label>
+            <input
+              type='number'
+              id='height'
+              value={this.state.height}
+              min={this.props.limits.minimumHeight} 
+              max={this.props.limits.maximumHeight} 
+              onChange={this.onChangeHeight}/>
+          </div>
+        </div>
+      </div>);
   }
 
   private renderRectangle = (rectangle: Rectangle, index: number) => {
@@ -89,14 +139,35 @@ export class FlexLayoutViewer extends React.Component<Properties> {
       </div>);
   }
 
+  private onChangeWidthCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({isWidthEnabled: event.target.checked});
+  }
+
+  private onChangeWidth = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({width: event.target.valueAsNumber});
+  }
+
+  private onChangeHeightCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({isHeightEnabled: event.target.checked});
+  }
+
+  private onChangeHeight = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({height: event.target.valueAsNumber});
+  }
+
   private static readonly CONSTRAINT_COLORS = {
     [Constraint.FIXED]: '#FFBB00',
     [Constraint.FILL_SPACE]: '#0066FF',
     [Constraint.FIT_CONTENT]: '#00BF2D',
   };
   private static readonly STYLE = {
-    container: {
+    wrapper: {
       height: '100%',
+      display: 'flex',
+      flexDirection: 'column'
+    } as React.CSSProperties,
+    container: {
+      height: 'calc(100% - 30px)',
       display: 'flex',
       alignContent: 'flex-start'
     } as React.CSSProperties,
@@ -113,6 +184,18 @@ export class FlexLayoutViewer extends React.Component<Properties> {
       height: '100%',
       display: 'grid',
       placeItems: 'center'
+    } as React.CSSProperties,
+    inputContainer: {
+      height: 30,
+      display: 'flex',
+      flexWrap: 'wrap',
+      alignItems: 'center',
+      columnGap: 30,
+      marginTop: 'auto'
+    } as React.CSSProperties,
+    inputGroup: {
+      display: 'flex',
+      alignItems: 'center'
     } as React.CSSProperties
   };
 }
